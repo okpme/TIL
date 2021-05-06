0b459bcbb1e6617fab931962357621ac0a064799
okpme <koimn4739@gmail.com>
2021-05-06 15:46:34 +0900

    dreamCoding 101

--- dreamCoding_101/당근 게임 프로젝트/js/main.js
+++ dreamCoding_101/당근 게임 프로젝트/js/main.js
@@ -1,103 +1,61 @@
-const   container = document.querySelector('#img_wrap'),
-    controller = document.querySelector('#controller'),
-    timeBtn = document.querySelector('#time');
+'use strict';
+const plusBtn = document.querySelector('#plus_button'),
+    items = document.querySelector('ul.items'),
+    item_row = document.querySelector('.item_row'),
+    itemName = document.querySelector('.item_name'),
+    input = document.querySelector('#footer_input');
 
- // 타이머
-let sec = 5;
-function setTime(){
-    let timer = setInterval(() => {
-        timeBtn.textContent = sec;
-        sec--;
-    
-        (sec === 0) && (timeBtn.textContent = `시간 초과`);
-        (sec === 0) && clearInterval(timer);
-    }, 1000);
+function printItemName(){
+    const text = input.value;
+    if(text === ''){
+        input.focus();
+        return;
+    }
+    const item = createElem(text);
+    items.appendChild(item);
+
+    item.scrollIntoView({
+        behavior: "smooth", 
+        block: "end",
+    });
+
+    input.value = '';
+    input.focus();
 }
 
-
-// carrot, bug 생성기함수
 let id = 0;
-let idd = 0;
+function createElem(text) {
+    const item_row = document.createElement('li');
+    item_row.setAttribute('class','item_row');
+    item_row.setAttribute('data-id',id);
+    item_row.innerHTML = `
+    <li class="item_row">
+        <div class="item">
+            <span class="item_name">${text}</span>
+            <button class="item_delete">
+                <img src="images/ic_trash_24.png" alt="" data-trash = ${id}>
+            </button>
+        </div>
+        <div class="item_divider"></div>
+    </li>`;
+    id++;
+    return item_row;
+}
 
-    function createCarrot(){
-        const carrot = document.createElement('button');
-        carrot.setAttribute('class','carrot');
-        carrot.innerHTML = `<img src="images/carrot.png" alt="" data-id= ${id}>`;
-        container.appendChild(carrot);
-            
-        carrot.style.left =  `${getRandomX()}%`;
-        carrot.style.top =  `${getRandomY()}%`;
-    
-        id++
-        return carrot;
-    }
-
-    function createBug(){
-        const bug = document.createElement('button');
-        bug.setAttribute('class','bug');
-        bug.innerHTML = `<img src="images/bug.png" alt="" data-bug= ${idd} >`;
-        container.appendChild(bug);
-            
-        bug.style.left =  `${getRandomX()}%`;
-        bug.style.top =  `${getRandomY()}%`;
-        
-        idd++
-        return bug;
-    }
-
-    //carrot과 bug 랜덤으로 배치함수
-    function getRandomX() {
-        let num = Math.floor(Math.random()*100);
-        if(num <= 100){
-            return num
-        } else{
-            return getRandomX();
-        }
-    }
-
-    function getRandomY() {
-        let num = Math.floor(Math.random()*100);
-        if(num >= 60){
-            return num
-        } else{
-            return getRandomY();
-        }
-    }
-
-document.addEventListener('click',event =>{
-    const setStartBtn = event.target.dataset.ctrl;
-    if(setStartBtn){
-        createCarrot();
-        createCarrot();
-        createCarrot();
-        
-        createBug();
-        createBug();
-        createBug();
-    }
+plusBtn.addEventListener('click',() => {
+    printItemName();
 });
 
+input.addEventListener('keypress', event => {
+    if(event.key === 'Enter'){
+        printItemName();
+    }
+}); 
 
-// carrot 클릭시 carrot 제거, alert bug실패
-document.addEventListener('click', event =>{
-    event.target === controller && setTime();
-
-    const test = event.target.dataset.id;
-    const test1 = event.target.dataset.bug;
-
+items.addEventListener('click', event => {
+    const test = event.target.dataset.trash;
     if(test){
-        let toBeDeleted = document.querySelector(`.carrot > img[data-id="${test}"]`);
-        toBeDeleted.parentNode.remove();
-        toBeDeleted.remove();
-        if(document.querySelectorAll('.carrot').length === 0){
-            alert('축하해요 완료');
-            container.remove();
-        }
+    let toBeDeleted = document.querySelector(`.item_row[data-id="${test}"]`);
+    toBeDeleted.remove();
     }
-
-    if(test1){
-        container.remove();
-        alert('다시해요');
-    }
-
-});
+})
